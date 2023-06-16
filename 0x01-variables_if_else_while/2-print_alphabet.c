c3eef01401dd3bd5660909aa49add622819ce7cc
DawkinsNderitu <solomonnderitu99@gmail.com>
2023-06-16 13:52:43 -0700

    rogram that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+
+/**
+ *  main - The entrypoint to the program
+ *
+ *  Return: It is always zero (Succes)
+ */
+
+int main(void)
+{
+	char lettr = 'a';
+
+	while (lettr <= 'z')
+	{
+		putchar(lettr);
+		lettr++;
+	}
+
+	putchar('\n');
+
+	return (o);
+
+}
