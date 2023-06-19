e73bbf6c0ad7fe2f9df9d58c50d474be87d9fdc2
DawkinsNderitu <solomonnderitu99@gmail.com>
2023-06-19 09:29:15 -0700

    programs that print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ /**
+  * main - Thenrey piont
+  *
+  * Return: Always succes
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+
+}
