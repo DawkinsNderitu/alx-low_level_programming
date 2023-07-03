d06ba40a520224756228ba6c52f076636eaeb42f
DawkinsNderitu <solomonnderitu99@gmail.com>
2023-07-02 19:22:36 -0700

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,26 @@
 #include "main.h"
 
 /**
- * _strncat - concanetes the two strings
- * @dest: The string to be appended upon
- * @src: The string that will get appended to dest
- * Return: A pointer to the resulting string dest
+ * _strncat - Copies a string up to a given number of characters
+ * @dest: The destination string where the copy will be stored
+ * @src: The source string to be copied
+ * @n: The maximum number of characters to be copied
+ *
+ * Description: This function copies characters from the source string @src
+ * to the destination string @dest. It will copy at most @n characters from
+ * @src, and if the end of @src is reached before @n characters are copied,
+ * the remaining characters in @dest will be filled with null terminators.
+ *
+ * Return: A pointer to the destination string @dest.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int x = 0, y = O;
+	int x = 0, y = 0;
 
-	while (src[x++])
+	while (dest[x++])
 	y++;
-
-	for (x = 0; y && x < n; x)
-	dest[x] = src[x];
-
-	for (x = y; x < n; x++)
-	dest(x) = '\0';
-
+	for (x = 0; src[x] && x < n; x++)
+	dest[y++] = src[x];
 	return (dest);
 }
