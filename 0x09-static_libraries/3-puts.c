71e44383dcfcf31e7d2da504ef1d4ec71c924268
DawkinsNderitu <solomonnderitu99@gmail.com>
2023-07-07 12:32:10 -0700

    static library libmy.a containing all the functions listed below

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,17 @@
 #include "main.h"
+
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
-int _islower(int c)
+
+void _puts(char *s)
 {
-	return (c >= 'a' && c <= 'z');
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
 }
