5e37c50ae6a0a0a4993ed117847d7fa9ba54ffe7
DawkinsNderitu <solomonnderitu99@gmail.com>
2023-07-09 23:25:21 -0700

    program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("The programs name is : %s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h> /* Include necessary standard library headers */
+
+/* Function prototypes */
+int _putchar(char c);
+void print_program_name(char *name);
+void print_argument_count(int count);
+void print_arguments(char **args);
+int multiply(int a, int b);
+int add_positive_numbers(int argc, char **argv);
+
+#endif /* MAIN_H */
